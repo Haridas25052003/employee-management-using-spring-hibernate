9e2d15b9cd0c49c4521de7f5a0f3968c227698ce
Haridas250503 <haridasshinde314@gmail.com>
2026-01-02 00:32:20 +0530

    add the changes

--- src/main/java/spring_project6/spring_project6/App.java
+++ src/main/java/spring_project6/spring_project6/App.java
@@ -25,29 +25,7 @@ public class App
         EmployeeService es=context.getBean(EmployeeService.class);
         
         Random r=new Random();
-        
-        String[] names = {
-    		    "Aarav", "Amit", "Anil", "Arjun", "Ashok",
-    		    "Bhavesh", "Bharat", "Chetan", "Deepak", "Dinesh",
-    		    "Ganesh", "Gaurav", "Harish", "Hemant", "Irfan",
-    		    "Jatin", "Karan", "Kishore", "Krishna", "Lokesh",
-    		    "Mahesh", "Manoj", "Mayur", "Mukesh", "Nagesh",
-    		    "Narendra", "Nikhil", "Pankaj", "Prakash", "Rahul",
-    		    "Rajesh", "Ramesh", "Ravi", "Rohit", "Sachin",
-    		    "Sagar", "Sandeep", "Sanjay", "Santosh", "Shivam",
-    		    "Shubham", "Siddharth", "Sunil", "Suraj", "Swapnil",
-    		    "Tanmay", "Uday", "Vaibhav", "Vikas", "Vijay",
-    		    "Vinay", "Vishal", "Yash", "Abhishek", "Aditya",
-    		    "Ajay", "Akash", "Akhil", "Anoop", "Ankit",
-    		    "Ankur", "Apoorva", "Aryan", "Atul", "Avinash",
-    		    "Chandrakant", "Darshan", "Dhruv", "Girish", "Hardik",
-    		    "Jay", "Jeevan", "Kunal", "Milan", "Mohit",
-    		    "Nitin", "Omkar", "Parth", "Piyush", "Rajat",
-    		    "Rohini", "Sameer", "Shailesh", "Tejas", "Yogesh"
-    		};
-        String[] designations = {"Developer", "Tester", "UI/UX", "Backend", "Support"};
-        String[] companies = {"TCS", "Infosys", "Wipro", "Capgemini", "Accenture"};
-
+    
         Employee e=new Employee();
         e.setName("haridas");
         e.setDesignation("ui/ux");
